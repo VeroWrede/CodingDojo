c17343d0277f2ef79204dbba2de7b2e50ff4ab84
Veronika Wrede <VWrede@earthnetworks.com>
2018-04-19 21:54:18 +0100

    adding array exercise

--- assignments/webFundamentals/js/display array.js
+++ assignments/webFundamentals/js/display array.js
@@ -1,10 +1,18 @@
-function displayArr(ary) {
-    for (var idx = 0; idx < ary.length; idx++){
-        console.log(idx, ' -> ', ary[idx]);
-        
+function displayArr(ary, symb, param) {
+    if (param == true){
+        for(var idx = ary.length-1; idx >= 0; idx--){
+            console.log(idx, symb, ary[idx]); 
+        }
     }
+    else{ 
+        for (var idx = 0; idx < ary.length; idx++){
+            console.log(idx, symb, ary[idx]);
+            
+        }
+    } 
+    
 }
 
 ary1 = ['Mina', 'Pat', 'Lennard'];
 
-displayArr(ary1)
+displayArr(ary1, '**',true)
