59de0bb13d8276ec3c461236642a6098f9f81743
VWrede <vwrede@earthnetworks.com>
2018-05-27 22:09:59 -0400

    starting firneds project
    DB communications exercise

--- assignments/python/pythonStack/myEnvs/formTest/server.py
+++ assignments/python/pythonStack/myEnvs/formTest/server.py
@@ -18,7 +18,7 @@ def create_user():
    return redirect('/show')
 @app.route('/show')
 def show_user():
-  return render_template('user.html', name = session['name'], email = session['email'])
+  return render_template('user.html')
 
 
 app.run(debug=True) # run our server
