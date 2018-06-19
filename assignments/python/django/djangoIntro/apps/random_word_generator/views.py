e3edbecc3d9ee557476082a44158719a57ad76a5
VWrede <vwrede@earthnetworks.com>
2018-06-18 21:26:21 -0400

    adding work to the random word generator app and starting survey form app

--- assignments/python/django/djangoIntro/apps/random_word_generator/views.py
+++ assignments/python/django/djangoIntro/apps/random_word_generator/views.py
@@ -2,8 +2,17 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 
 # Create your views here.
 
-def index(request):
+def index(request, data):
 	return render(request, 'random_word_generator/index.html')
+
+def generate(request):
+	data = {
+	'number': i,
+	'word': get_random_string(length=14)
+	}
+	return redirect(request, 'random_word_generator/index.html')
+
