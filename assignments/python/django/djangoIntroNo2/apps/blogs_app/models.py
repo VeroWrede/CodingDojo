438190cd0615994b84e59f23c5c4992f905406f2
VWrede <vwrede@earthnetworks.com>
2018-08-01 15:58:19 -0400

    adding blog_app models

--- assignments/python/django/djangoIntroNo2/apps/blogs_app/models.py
+++ assignments/python/django/djangoIntroNo2/apps/blogs_app/models.py
@@ -3,4 +3,17 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class blog(models.Model):
+	owner = models.CharField(max_length = 200)
+	name = models.CharField(max_length = 200)
+	cathegory = models.CharField(max_length = 200)
+	created_at = models.dateTimeField(auto_now_add = True)
+	updated_at = models.dateTimeField(auto_now = True)
+
+class blog_post(models.Model):
+	author = models.CharField(max_length = 200)
+	title = models.CharField(max_length = 200)
+	content = models.CharField(max_length = 1000)
+	created_at = models.dateTimeField(auto_now_add)
+	updated_at = models.dateTimeField(auto_now)
+	blog = models.ForeignKey(blog, related_name = 'blogs')
