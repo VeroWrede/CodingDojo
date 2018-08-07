3ec920cd58b86276c8bce807b934352d4b08c2f9
VWrede <vwrede@earthnetworks.com>
2018-08-06 20:38:53 -0400

    starting timeDisplay hw

--- assignments/python/django/djangoIntroNo2/apps/blogs_app/models.py
+++ assignments/python/django/djangoIntroNo2/apps/blogs_app/models.py
@@ -3,13 +3,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class blogManager(models.Model):
+	def validate_and create post(salf, form_data):
+		errors = []
+
+		if !name:
+			errors.append('no name')
+		
+		if errors:
+			return(False, errors)
+		blog_post = self.create(content = form_data['content'], author = form_data['autor'])
+		return (true, blog_post)
+
 class blog(models.Model):
 	owner = models.CharField(max_length = 200)
 	name = models.CharField(max_length = 200)
 	cathegory = models.CharField(max_length = 200)
 	created_at = models.dateTimeField(auto_now_add = True)
 	updated_at = models.dateTimeField(auto_now = True)
-
+	objects = blogManager()
+	
 class blog_post(models.Model):
 	author = models.CharField(max_length = 200)
 	title = models.CharField(max_length = 200)
--- assignments/python/django/djangoIntroNo2/apps/blogs_app/views.py
+++ assignments/python/django/djangoIntroNo2/apps/blogs_app/views.py
@@ -3,13 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, HttpResponse
 
+from models import *
+
+form django palce import messages to display errors 
+
 def index(request):
-	return render(request, 'blogs_app/index.html')
+	context = {
+		'blogs_posts' : Blog.object.all()
+	}
+	return render(request, 'blogs_app/index.html', context)
 
 def new(request):
 	return render(request, 'blogs_app/new.html')
 
 def create(request):
+	if request.method == 'POST':
+		valid, response = Blog.objects.Validate_andCreate_post(request.POST)
+		if not valid:
+			messages.error(error)
+			return redirect('/new')
+		do blabla
+	return redirect('/')
+	Blog.Validate_andCreate_post
 	return render(request, 'blogs_app/create.html')
 
 def show(request, blog_num):
