65c75284e728bb61c32cb65d86d0f20b2b2ecc13
Vero Wrede <vwrede@decisionhealth.com>
2018-09-10 17:02:10 -0400

    finishing basic13

--- assignments/netCore/Basic13/Program.cs
+++ assignments/netCore/Basic13/Program.cs
@@ -7,16 +7,113 @@ namespace Basic13
         static void Main(string[] args)
         {
 
-            Avg(new int[] {255, 7890 ,123 ,43 ,6 ,1});
+            NtoS(new object[] { 2, 2, -4, 02, -908});
             Console.WriteLine("Hello World!");
         }
 
-        public static void OddArr(int num) {
-            
-            for (int i = 0; i <= num; i++) {
-                if (i % 2 != 0) {
-                    Console.WriteLine(i);
+        public static void NtoS(object[] arr) {
+            object Dojo = "Dojo";
+            int i = 0;
+            foreach(object num in arr) {
+                if (num is int && (int)num < 0) {
+                    arr[i] = Dojo;
+                    i ++;
+                } else {
+                    i ++;
                 };
+            };
+            foreach(object num in arr) {
+                Console.WriteLine(num);
+            };
+        }
+
+        public static void Shift(int[] arr) {
+            int i = 0;
+            while ( i < arr.Length - 1) {
+                arr[i] = arr[i+1];
+                i ++;
+            };
+            arr[arr.Length - 1] = 0;
+
+            foreach(int num in arr) {
+                Console.WriteLine(num);
+            };
+        }
+
+        public static void MMA(int[] arr) {
+            int Max = arr[0];
+            int Min = arr[0];
+            int Avg;
+            int temp = 0;
+            foreach (int num in arr) {
+                temp += num;
+                if (num < Min) {
+                    Min = num;
+                } else if (num > Max) {
+                     Max = num;
+                } else {
+                    continue;
+                };
+            };
+            Avg = temp / arr.Length;
+            Console.WriteLine(Max);
+            Console.WriteLine(Min);
+            Console.WriteLine(Avg);
+        }
+
+        public static void Negs(int[] arr) {
+            int i = 0;
+            foreach (int num in arr) {
+                if (num < 0 ) {
+                    arr[i] = 0;
+                };
+                i ++;
+            };
+            foreach ( int num in arr) {
+                Console.WriteLine(num);
+            };
+        }
+
+        public static void Sqaure(int[] arr) {
+            int i = 0;
+            foreach(int num in arr) {
+                arr[i] = num*num;
+                i ++;
+            };
+            foreach(int num in arr) {
+            Console.WriteLine(num);
+            };
+        }
+
+        public static void GreaterThan(int limit, int[] arr) {
+            int count = 0;
+            foreach(int num in arr) {
+                if (num > limit) {
+                    count ++;
+                };
+            };
+            Console.WriteLine(count);
+        }
+
+        public static void OddArr(int num) {
+            int index = 0;
+            int j = 0;
+            int len;
+            if (num % 2 == 0) {
+                len = num /2;
+            } else {
+                len = (num + 1) /2;
+            }
+            int[] result = new int[len];           
+            while ( index < len) {
+                if ( j % 2 != 0) {
+                    result[index] = j;
+                    index ++;
+                };
+                j ++;
+            };
+            foreach (int number in result) {
+            Console.WriteLine(number);
             };
         }
 
