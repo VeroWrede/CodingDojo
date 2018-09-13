7fda62c3d3cf5f7660042c1b3d056fe334445265
Vero Wrede <vwrede@decisionhealth.com>
2018-09-13 16:42:44 -0400

    finish inheritance

--- assignments/netCore/Inheritance/Program.cs
+++ assignments/netCore/Inheritance/Program.cs
@@ -6,7 +6,19 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human h = new Human("Mina");
+            Wizard w = new Wizard("Nate");
+            Ninja n = new Ninja("Wes");
+            Console.WriteLine(h.health);
+            Console.WriteLine(w.health);
+            w.heal();
+            w.firebal(h);
+            Console.WriteLine(n.health);
+            n.get_away();
+            Console.WriteLine(n.health);
+            
+           
+
         }
     }
 }
